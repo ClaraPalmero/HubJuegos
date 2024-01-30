d033d8b4a2e77b5e63ffe4c110fa07697e420462
clara <clarapalmero@email.com>
2024-01-30 18:04:31 +0100

    css ahorcado imagen

--- hubjuegos/src/pages/Ahorcado/Ahorcado.js
+++ hubjuegos/src/pages/Ahorcado/Ahorcado.js
@@ -2,21 +2,20 @@ import { PrintBotonLetras } from "../../components";
 import { game_over, iniciar } from "../../utils";
 import "./Ahorcado.css";
 
-//!----------------------------------TEMPLETE---------------------------------------------------
-
-//!creo un templete con el body del juego para añadirlo a mi HTML
+//? ---1--- creo un templete con el body del juego para añadirlo a mi HTML ---
 
 const template = () => `
-<img id="imagen" src="img/img0.png" alt="Ahorcado" />
+<div> <img id="imagen" src="img/img0.png" alt="Ahorcado" />
 <div>
       <p id="palabra_a_adivinar"></p>
       <button id="jugar">Obtener palabra</button>
 
       <p id="resultado"></p>
       <div id="letras"></div> 
+</div>
 </div>`;
 
-//? pinto mi template en el html. Para eso le digo que me lo meta en app con el querySelector y con app.innerHTMl (esto porque es texto) le estoy diciendo que me meta el templete al HTML
+//? ---2--- pinto mi template en el html. Para eso le digo que me lo meta en app con el querySelector y con app.innerHTMl (esto porque es texto) le estoy diciendo que me meta el templete al HTML
 
 const listeners = () => {
   const btn = document.getElementById("jugar");
